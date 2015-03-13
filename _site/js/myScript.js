c4e1cea035b1c8a9c1ee3d46a2cbd8978e4ee8bc
Thomas Flores <thomas.flores84@eagles.ewu.edu>
2015-03-13 07:58:09 -0700

    njnjk

--- _site/js/myScript.js
+++ _site/js/myScript.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $('.cardwrapper').height(
 		$('.cardwrapper').width() * 1.44
 	);
@@ -35,3 +36,46 @@ $('.colorPicker').height($('.top-bar').height());
 $('.mainBody').css('font-size', 
 		($('.cardwrapper').width() / 300 * 12) + 'pt'
 	);
+=======
+function modifySizes () {
+	$('.cardwrapper').outerHeight(
+			$('.cardwrapper').outerWidth() * 1.395
+		);
+	
+	$('.TitleCostRow, .TypeRarityRow, .attack').css(
+		'background-color',
+		function () {
+			var parts = $('.cardBorder').css('background-color').match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+			for (i = 1; i < parts.length; i++) {
+				parts[i] = parseInt(parts[i]) + parseInt((255 - parts[i]) * 0.7);
+				if (parts[i] > 255) parts[i] = 255;
+			}
+			return 'rgb(' + parts[1] + ',' + parts[2] + ',' + parts[3] + ')';
+		});
+	
+	$('.RulesFlavorRow').css(
+		'background-color',
+		function () {
+			var parts = $('.cardBorder').css('background-color').match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+			for (i = 1; i < parts.length; i++) {
+				parts[i] = parseInt(parts[i]) + parseInt((255 - parts[i]) * 0.9);
+				if (parts[i] > 255) parts[i] = 255;
+			}
+			return 'rgb(' + parts[1] + ',' + parts[2] + ',' + parts[3] + ')';
+		});
+	
+	$('.colorPicker').height($('.top-bar').height());
+	
+	$('.cardwrapper div, .RulesFlavorRow *').css('font-size', 
+			($('.cardwrapper').width() / 300 * 12) + 'pt'
+		);
+	
+	$('.RulesFlavorRow').height(
+			$('.cardBorder').height() - 
+			$('.TitleCostRow').outerHeight(true) - 
+			$('.TypeRarityRow').outerHeight()
+		);
+}
+modifySizes();
+$( window ).resize(modifySizes);
+>>>>>>> FETCH_HEAD
