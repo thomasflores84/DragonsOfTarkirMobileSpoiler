a37f0c2ed3cd5ac32b5bea2a7070aacb0cb188af
thomasflores84 <thomas.flores84@eagles.ewu.edu>
2015-04-03 12:24:45 -0700

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -9,28 +9,6 @@ $('.RulesFlavorRow').height(
 		$('.TypeRarityRow').outerHeight()
 	);
 
-$('.TitleCostRow, .TypeRarityRow, .attack').css(
-	'background-color',
-	function () {
-		var parts = $('.cardBorder').css('background-color').match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-		for (i = 1; i < parts.length; i++) {
-			parts[i] = parseInt(parts[i]) + parseInt((255 - parts[i]) * 0.7);
-			if (parts[i] > 255) parts[i] = 255;
-		}
-		return 'rgb(' + parts[1] + ',' + parts[2] + ',' + parts[3] + ')';
-	});
-
-$('.RulesFlavorRow').css(
-	'background-color',
-	function () {
-		var parts = $('.cardBorder').css('background-color').match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-		for (i = 1; i < parts.length; i++) {
-			parts[i] = parseInt(parts[i]) + parseInt((255 - parts[i]) * 0.9);
-			if (parts[i] > 255) parts[i] = 255;
-		}
-		return 'rgb(' + parts[1] + ',' + parts[2] + ',' + parts[3] + ')';
-	});
-
 $('.colorPicker').height($('.top-bar').height());
 
 $('.cardwrapper div, .RulesFlavorRow *').css('font-size', 
@@ -41,28 +19,6 @@ function modifySizes () {
 	$('.cardwrapper').outerHeight(
 			$('.cardwrapper').outerWidth() * 1.395
 		);
-	
-	$('.TitleCostRow, .TypeRarityRow, .attack').css(
-		'background-color',
-		function () {
-			var parts = $(this).parent().css('background-color').match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-			for (i = 1; i < parts.length; i++) {
-				parts[i] = parseInt(parts[i]) + parseInt((255 - parts[i]) * 0.7);
-				if (parts[i] > 255) parts[i] = 255;
-			}
-			return 'rgb(' + parts[1] + ',' + parts[2] + ',' + parts[3] + ')';
-		});
-	
-	$('.RulesFlavorRow').css(
-		'background-color',
-		function () {
-			var parts = $(this).parent().css('background-color').match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-			for (i = 1; i < parts.length; i++) {
-				parts[i] = parseInt(parts[i]) + parseInt((255 - parts[i]) * 0.9);
-				if (parts[i] > 255) parts[i] = 255;
-			}
-			return 'rgb(' + parts[1] + ',' + parts[2] + ',' + parts[3] + ')';
-		});
 	
 	$('.colorPicker').height($('.top-bar').height());
 	
