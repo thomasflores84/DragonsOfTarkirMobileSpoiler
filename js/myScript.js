12133ceb9dbc63c8f3a34c944c12bb460c314537
Thomas Flores <thomas.flores84@eagles.ewu.edu>
2015-04-07 21:23:38 -0700

    fgdgfsgfds

--- js/myScript.js
+++ js/myScript.js
@@ -1,13 +1,12 @@
 
-$('.site').css('font-size', 
-		($('.cardwrapper').width() / 300 * 12) + 'pt'
-	);
-
 function modifySizes () {
 	
-	$('.site').css('font-size', 
+    $('.site').css('font-size', 
+        ($('#cardRuler').outerWidth() / 425) + 'em'
+      );
+	/*$('.site').css('font-size', 
 			($('.cardwrapper').width() / 300 * 12) + 'pt'
-		);
+		);*/
 }
-
+modifySizes ();
 $( window ).resize(modifySizes);
