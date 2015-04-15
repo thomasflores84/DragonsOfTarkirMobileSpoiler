eca03ef4e6a1a3518d78f8302b0873d54eab0dfb
thomasflores84 <thomas.flores84@eagles.ewu.edu>
2015-04-15 09:48:16 -0700

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -1,18 +1,15 @@
 var isLoaded = false;
 function modifySizes () {
-	
     $('.site').css('font-size', 
         ($('.TitleCostRow').outerHeight() * 2  / 5) + 'px'
       );
-
-	if (!isLoaded) {
-		alert($('input[type=hidden]').toArray());
-		isLoaded = true;
-	}
 }
 
 $( window ).resize(modifySizes);
 
-window.onload =  $('.site').css('font-size', 
-        ($('.TitleCostRow').outerHeight() * 2  / 5) + 'px'
-      );
+window.onload =  function () {
+	$('.site').css('font-size', 
+		($('.TitleCostRow').outerHeight() * 2  / 5) + 'px'
+	);
+	alert($('input[type=hidden]').toArray());
+}
