20dcf8c5fee59f4c93f5bb489fb6c920378c2c6b
thomasflores84 <thomas.flores84@eagles.ewu.edu>
2015-04-15 09:40:45 -0700

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -5,8 +5,11 @@ function modifySizes () {
         ($('.TitleCostRow').outerHeight() * 2  / 5) + 'px'
       );
 
-	if (!isLoaded) alert($('input[type=hidden]').toArray());
-	isLoaded = true;
+	if (!isLoaded) {
+		alert($('input[type=hidden]').toArray());
+		isLoaded = true;
+		$( window ).resize(modifySizes);
+	}
 }
-$( window ).resize(modifySizes);
+
 window.onload = modifySizes ();
