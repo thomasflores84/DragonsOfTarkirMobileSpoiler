6b32270d52174a1425a1015e48ec8d012e5a673c
Thomas Flores <thomas.flores84@eagles.ewu.edu>
2015-04-16 08:54:57 -0700

    Update from work

--- _site/js/myScript.js
+++ _site/js/myScript.js
@@ -1,12 +1,33 @@
-
+var isLoaded = false;
 function modifySizes () {
-	
     $('.site').css('font-size', 
-        ($('#cardRuler').outerWidth() / 425) + 'em'
+<<<<<<< Updated upstream
+        ($('.TitleCostRow').outerHeight() * 2  / 5) + 'px'
+=======
+        ($('#cardRuler').outerWidth() / 450) + 'em'
+>>>>>>> Stashed changes
       );
-	/*$('.site').css('font-size', 
-			($('.cardwrapper').width() / 300 * 12) + 'pt'
-		);*/
 }
-modifySizes ();
+
 $( window ).resize(modifySizes);
+
+window.onload =  function () {
+	$('.site').css('font-size', 
+		($('.TitleCostRow').outerHeight() * 2  / 5) + 'px'
+	);
+	loadImagesInSequence($('input[type=hidden]').toArray());
+}
+
+function loadImagesInSequence(inputHiddenImages) {
+	if (!inputHiddenImages.length) {
+		return;
+	}
+	
+	var imgInput = inputHiddenImages.shift();
+	var img = new Image();
+	
+	img.onload = function(){ loadImagesInSequence(inputHiddenImages) };
+	img.src = imgInput.value;
+	
+	imgInput.parentNode.appendChild(img);
+}
